c3a45d2ccb7c9edc5ea81d8907e36fe02f69f66e
ArthurHsing <573105494@qq.com>
2021-12-01 10:18:35 +0800

    任务大小改变，完成

--- main.m
+++ main.m
@@ -2,27 +2,12 @@ clear all;close all;clc;
 setSystemConfig();
 
 
-[changeRateResult] = arriveRateChange(); %任务到达率的改变
-arriveRateChange_draw(changeRateResult); %画图
+% [changeRateResult] = arriveRateChange(); %任务到达率的改变
+% arriveRateChange_draw(changeRateResult); %画图
+
+[changeTaskSizeResult] = taskSizeChange(); %任务到达率的改变
+taskSizeChange_draw(changeTaskSizeResult); %画图
 
 % global systemConfig;
-% systemConfig.deviceArrivalRate = ones(1, systemConfig.deviceNum).*30;
-% systemConfig.taskComputationIntensityPerBit = 1;
+% systemConfig.taskSize = (1*10e6).*(2);
 % myOffload();
-
-% global systemConfig;
-% systemConfig.deviceArrivalRate = ones(1, systemConfig.deviceNum).*1;
-% averageCompletionTime = mmssOffload();
-% disp(averageCompletionTime);
-
-% global systemConfig;
-% systemConfig.deviceArrivalRate = ones(1, systemConfig.deviceNum).*1;
-% allInEdgeOffload();
-
-% global systemConfig;
-% systemConfig.deviceArrivalRate = ones(1, systemConfig.deviceNum).*1;
-% allInCloudOffload();
-
-% global systemConfig;
-% systemConfig.deviceArrivalRate = ones(1, systemConfig.deviceNum).*1;
-% randomOffload();
