ff1f2a6bdb3d2b35818d26d445dfbb875b2e4569
幸成 <573105494@qq.com>
2022-01-11 15:58:23 +0800

    蝴蝶优化算法(BOA)应用完毕

---
+++ getStrategy_BOA.m
@@ -0,0 +1,36 @@
+function [] = getStrategy_BOA()
+    global systemConfig;
+    global bestOffloadNumResult_BOA;
+    N=10; % 种群大小
+%     N = 30;
+%     fobj = @F1;
+    fobj = @fitnessfun;
+    deviceNum = systemConfig.deviceNum;
+    dim = deviceNum + 1; % 维度，设备个数加上一个边缘节点的个数
+%     dim = 30;
+    edgeNum = systemConfig.edgeNum; %边缘服务器个数
+    T= 1000; %迭代次数
+%     T = 500;
+    Lb=[ones(1, deviceNum) edgeNum];
+%     Lb = -100;
+    Ub=[ones(1, deviceNum)*50 100];
+%     Ub = 100;
+    [Best_score,Best_pos,cg_curve, FRBest]=BOA(N,T,Lb,Ub,dim,fobj);
+    bestOffloadNumResult_BOA.FRBest = FRBest;
+    semilogy(cg_curve,'Color','r')
+    title('Convergence curve')
+    xlabel('Iteration');
+    ylabel('Best score obtained so far');
+
+    axis tight
+    grid off
+    box on
+    legend('BOA')
+
+    display(['The best solution obtained by BOA is : ', num2str(Best_pos)]);
+    display(['The best optimal value of the objective funciton found by BOA is : ', num2str(Best_score)]);
+end
+
+function o = F1(x)
+o=sum(x.^2);
+end
--- main.m
+++ main.m
@@ -2,7 +2,7 @@ clear all;close all;clc;
 setSystemConfig();
 global bestOffloadNumResult;
 global systemConfig;
-
+global bestOffloadNumResult_BOA;
 % systemConfig.taskSize = 2*8*1024*1024; % 1M bits
 % systemConfig.taskSize = 1.8*8*1024*1024; % 1M bits
 % [arrTimesAll, arrSrvTimeAll] = getArriveTimeAndSrvTime();
@@ -14,7 +14,7 @@ global systemConfig;
 % systemConfig.wireless.wireless_gains = ones(1, systemConfig.deviceNum); %各个设备与边缘节点的无线信道的信道增益
 % changeRateResultArr = struct([]);
 
-% % 第一个点做三组实验，每一个实验改变无线信道的传输速率
+% % 第一个点做三组实验，每一个实验改变无线信道的传输速率（任务到达率的改变）
 % for m = 1:3
 %     systemConfig.wireless.bandWidth = m*10e6; %带宽kMHz
 %     % 做多次实验求平均——任务到达率的变化
@@ -26,7 +26,7 @@ global systemConfig;
 %     arriveRateChange_draw(changeRateResult, m, 0); %画图
 % end
 
-% 第一个点做三组实验，每一个实验改变无线信道的传输速率
+% 第一个点做三组实验，每一个实验改变无线信道的传输速率（任务体积的改变）
 % for n = 1:3
 %     systemConfig.wireless.bandWidth = n*10e6; %带宽nMHz
 %     % 做多次实验求平均——任务体积的变化
@@ -38,24 +38,31 @@ global systemConfig;
 %     taskSizeChange_draw(changeTaskSizeResult, n, 0); %画图
 % end
 
-result = 0;
-corr_Aver = 0;
-max = 10;
-for q = 1 : max
-        systemConfig.wireless.wireless_gains = raylrnd(ones(...
-            1, systemConfig.deviceNum).*systemConfig.wireless.wireless_gain_parameter); %各个设备与边缘节点的无线信道的信道增益
-    getStrategy();
-    result = result + bestOffloadNumResult.finishTime .* (1/max);
-    corr_Aver = corr_Aver + bestOffloadNumResult.FRBest.correlation_delta .* (1/max);
-end
-disp(result);
-disp(corr_Aver);
+% 测试模拟退火改进HBA
+% result = 0;
+% corr_Aver = 0;
+% max = 1;
+% for q = 1 : max
+%         systemConfig.wireless.wireless_gains = raylrnd(ones(...
+%             1, systemConfig.deviceNum).*systemConfig.wireless.wireless_gain_parameter); %各个设备与边缘节点的无线信道的信道增益
+%     getStrategy();
+%     result = result + bestOffloadNumResult.finishTime .* (1/max);
+%     bestOffloadNumResult.FRBest.correlation_delta
+%     corr_Aver = corr_Aver + bestOffloadNumResult.FRBest.correlation_delta .* (1/max);
+% end
+% disp(result);
+% disp(corr_Aver);
+
+
 % [wirelessChannelChangeResult] = wirelessChannelChange(); %信道的波动
 % wirelessChannelChange_draw(wirelessChannelChangeResult); %画图
 % capacityResult = 
 
 % systemConfig.taskSize = (1*10e6).*(2);
 % myOffload();
+
+%测试其它算法
+getStrategy_BOA();
 
 function [average]  = getAverageOfSeveralExperimentTimes(allTimes)
     experimentTypeCell = fieldnames(allTimes);
